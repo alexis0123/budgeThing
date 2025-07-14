f2580b0f025bf226bf1dd2acdf88ab9a8dda518d
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-07-14 19:32:43 +0800

    Added the keys and format the amount all inside mainscreen

---
+++ app/src/main/java/com/budgething/ui/components/NumKey.kt
@@ -0,0 +1,21 @@
+package com.budgething.ui.components
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Key(
+    label: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.width(70.dp).height(70.dp)
+    ) { Text(label, fontSize = 25.sp) }
+}
--- app/src/main/java/com/budgething/ui/screens/MainScreen.kt
+++ app/src/main/java/com/budgething/ui/screens/MainScreen.kt
@@ -6,19 +6,30 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.budgething.ui.theme.BudgeThingTheme
 import com.budgething.R
+import com.budgething.ui.components.Key
+import com.budgething.ui.components.ReadOnlyTextField
 
 @Composable
 fun BoxScope.Background() {
@@ -42,18 +53,55 @@ fun MainScreen() {
 
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .padding(25.dp),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            val amount = remember { mutableStateOf("")}
+            val formattedAmount = amount.value.toLongOrNull()?.let {
+                "%,d".format(it) } ?: amount.value
             Box(
                 modifier = Modifier
-                    .width(100.dp)
-                    .height(55.dp)
-                    .background(MaterialTheme.colorScheme.secondary)
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(bottom = 20.dp),
+                contentAlignment = Alignment.BottomEnd
             ) {
-                Text("Hello, World")
+                Text("$formattedAmount | ", fontSize = 40.sp,
+                    color = MaterialTheme.colorScheme.onBackground)
             }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                val keypad = listOf(
+                    listOf("7", "8", "9"),
+                    listOf("4", "5", "6"),
+                    listOf("1", "2", "3"),
+                    listOf("0", ".", "")
+                )
+
+                keypad.forEach { row ->
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        row.forEach { label ->
+                            Key(label) {
+                                when (label) {
+                                    "" -> amount.value = amount.value.dropLast(1)
+                                    else -> amount.value += label
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 
