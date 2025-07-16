5effbcb1a15703db644db8cdb7b58250be2473a8
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-07-16 10:51:18 +0800

    Added viewmodel for numkey and refactor

---
+++ app/src/main/java/com/budgething/ViewModel/NumKeyViewModel.kt
@@ -0,0 +1,24 @@
+package com.budgething.ViewModel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class NumKeyViewModel: ViewModel() {
+
+    private val _amount = MutableStateFlow("")
+    val amount: StateFlow<String> get() = _amount
+
+    fun addDigit(num: String) {
+        _amount.value += num
+    }
+
+    fun deleteLast() {
+        _amount.value.dropLast(1)
+    }
+
+    fun deleteAll() {
+        _amount.value = ""
+    }
+
+}
